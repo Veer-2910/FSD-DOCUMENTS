c14abb8e5b72e80d546cf9aecc917f95b4729914
Veer-2910 <veerbhalodia29@gmail.com>
2025-10-06 13:15:05 +0530

    Delete Practical 18 directory

--- Practical 18/task18/backend/models/Note.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const noteSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-}, { timestamps: true });  // automatically adds createdAt & updatedAt
-
-export default mongoose.model("Note", noteSchema);
